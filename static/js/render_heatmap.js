026e1ef3aab8612edeb9bb592509bd8db0583ee8
nchakicherla <nchaki6@gmail.com>
2024-08-28 22:43:12 -0700

    update layout; small changes

--- static/js/render_heatmap.js
+++ static/js/render_heatmap.js
@@ -1,5 +1,4 @@
 function getColor(value) {
-    //console.log("in getColor")
     return value > 30 ? '#FF0000' :
            value > 20 ? '#FFA500' :
            value > 10 ? '#FFFF00' :
@@ -14,13 +13,13 @@ function style(feature) {
     };
 }
 
+// define mouseover and popup behavior
 function onEachFeatureFn(feature, layer) {
     if (feature.properties && feature.properties.country) {
-        //console.log("here")
         const formatted_prevalence = feature.properties.prevalence.toFixed(1) + "%"
 
         var popupContent = `
-            <div style="text-align: center;">
+            <div style="text-align: center">
                 <h3>${feature.properties.country}</h3>
                 <p><strong>${formatted_prevalence}</strong></p>
             </div>
@@ -40,23 +39,6 @@ function onEachFeatureFn(feature, layer) {
     });
 }
 
-function loadGeoJson(selected_dataset) {
-    if(currentGeoJsonLayer) {
-        map.removeLayer(currentGeoJsonLayer);
-    }
-
-    currentGeoJsonLayer = L.geoJSON(selected_dataset, {
-
-        onEachFeature: onEachFeatureFn,
-        pointToLayer: function(feature, latlng) {
-            return L.circleMarker(latlng, style(feature));
-        }
-
-    }).addTo(map);
-
-    map.fitBounds(L.geoJSON(selected_dataset).getBounds());
-}
-
 var map = L.map('map').setView([0, 0], 2);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -69,24 +51,40 @@ var currentGeoJsonLayer;
 // set overweight layer as default
 loadGeoJson(overweight_layer_geojson_data_gdf)
 
+// function called to reload GeoJSON data upon select menu usage
+function loadGeoJson(selected_dataset) {
+
+    if(currentGeoJsonLayer) {
+        map.removeLayer(currentGeoJsonLayer);
+    }
+    currentGeoJsonLayer = L.geoJSON(selected_dataset, {
+
+        onEachFeature: onEachFeatureFn,
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng, style(feature));
+        }
+
+    }).addTo(map);
+
+    map.fitBounds(L.geoJSON(selected_dataset).getBounds());
+}
+
 document.getElementById('geojson-select').addEventListener('change', function(event) {
 
     var selection = event.target.value;
 
     // depending on selection, load the appropriate data onto the map as a layer
-
-    if (selection == "overweight_layer_geojson_data_gdf") {
+    if (selection == "overweight") {
         loadGeoJson(overweight_layer_geojson_data_gdf)
-    } else if (selection == "stunting_layer_geojson_data_gdf") {
+    } else if (selection == "stunting") {
         loadGeoJson(stunting_layer_geojson_data_gdf)
-    } else if (selection == "underweight_layer_geojson_data_gdf") {
+    } else if (selection == "underweight") {
         loadGeoJson(underweight_layer_geojson_data_gdf)
-    } else if (selection == "wasting_layer_geojson_data_gdf") {
+    } else if (selection == "wasting") {
         loadGeoJson(wasting_layer_geojson_data_gdf)
-    } else if (selection == "wasting_severe_layer_geojson_data_gdf") {
+    } else if (selection == "wasting_severe") {
         loadGeoJson(wasting_severe_layer_geojson_data_gdf)
     } else {
         console.log("invalid selection!");
     }
 });
-
